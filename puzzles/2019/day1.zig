c3fa0df4032fcfbbe0166cd7850d5f77498bd28a
Dustin Sallings <dustin@spy.net>
2025-07-23 18:35:59 -1000

    2019/1

---
+++ puzzles/2019/day1.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+const aoc = @import("aoc");
+const intcode = @import("intcode.zig");
+
+fn fuelReq(i: i32) i32 {
+    return @divTrunc(i, 3) - 2;
+}
+
+test "part1" {
+    var answer: i32 = 0;
+    const T = struct {
+        answer: i32 = 0,
+
+        fn parseLine(s: *i32, line: []const u8) aoc.input.ParseError!bool {
+            s.* += fuelReq(try aoc.input.parseInt(i32, line));
+            return true;
+        }
+    };
+
+    try aoc.input.parseLines("input/2019/day1", &answer, T.parseLine);
+    try std.testing.expectEqual(3317659, answer);
+}
+
+test "part2" {
+    var answer: i32 = 0;
+
+    const T = struct {
+        answer: i32 = 0,
+
+        fn parseLine(s: *i32, line: []const u8) aoc.input.ParseError!bool {
+            const base = try aoc.input.parseInt(i32, line);
+            var fueld = fuelReq(base);
+            while (fueld > 0) {
+                s.* += fueld;
+                fueld = fuelReq(fueld);
+            }
+
+            return true;
+        }
+    };
+
+    try aoc.input.parseLines("input/2019/day1", &answer, T.parseLine);
+    try std.testing.expectEqual(4973616, answer);
+}
