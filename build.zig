35c559c054445178bc7cf34a23f89cab7b66095d
Dustin Sallings <dustin@spy.net>
2025-09-07 21:18:05 -1000

    Simplify build.zig

--- build.zig
+++ build.zig
@@ -1,18 +1,8 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) !void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     const aoc = b.addModule("aoc", .{
@@ -20,15 +10,8 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
     });
-
-    const zigthesis = b.dependency("zigthesis", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
     const test_step = b.step("test", "Run unit tests");
 
-    // Replace the hard-coded list with dynamic discovery from the filesystem.
     const Puzzle = struct {
         year: []const u8,
         day: []const u8,
@@ -117,19 +100,23 @@ pub fn build(b: *std.Build) !void {
         test_step.dependOn(&run_exe_unit_tests.step);
     }
 
+    const zigthesis = b.dependency("zigthesis", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
     const lib_unit_tests = b.addTest(.{
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/root.zig"),
             .target = target,
             .optimize = optimize,
+            .imports = &.{
+                .{ .name = "zigthesis", .module = zigthesis.module("zigthesis") },
+            },
         }),
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    lib_unit_tests.root_module.addImport("zigthesis", zigthesis.module("zigthesis"));
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     test_step.dependOn(&run_lib_unit_tests.step);
 }
