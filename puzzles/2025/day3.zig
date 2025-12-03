44526a4b05aa34defff534033f97e0d91ab58201
Dustin Sallings <dustin@spy.net>
2025-12-03 12:37:54 -1000

    day 3 part 2

--- puzzles/2025/day3.zig
+++ puzzles/2025/day3.zig
@@ -8,44 +8,37 @@ fn digit(g: aoc.grid.Grid, p: aoc.twod.Point) ?u8 {
     return null;
 }
 
+fn select(g: aoc.grid.Grid, start: aoc.twod.Point, want: i32, totes: u64) u64 {
+    if (want == 0) {
+        return totes;
+    }
+    const end = g.bounds.maxs[0] - want + 2;
+
+    const d = digit(g, start);
+    var maxp = start;
+    var maxv = d.?;
+    for (@as(usize, @intCast(start[0]))..@as(usize, @intCast(end))) |col| {
+        const p = .{ @as(i32, @intCast(col)), start[1] };
+        if (digit(g, p)) |dp| {
+            if (dp > maxv) {
+                maxp = p;
+                maxv = dp;
+            }
+            // Nothing gonna be bigger than this.
+            if (maxv == 9) {
+                break;
+            }
+        }
+    }
+    maxp[0] = maxp[0] + 1;
+    return select(g, maxp, want - 1, (totes * 10) + maxv);
+}
+
 fn part1(g: aoc.grid.Grid) u32 {
     var totes: u32 = 0;
     for (0..1 + @as(usize, @intCast(g.bounds.maxs[1]))) |row| {
-        const rowi: i32 = @intCast(row);
-        const d = digit(g, .{ 0, rowi });
-        var maxp: aoc.twod.Point = .{ 0, rowi };
-        var maxv = d.?;
-        for (0..@as(usize, @intCast(g.bounds.maxs[0]))) |col| {
-            const p = .{ @as(i32, @intCast(col)), rowi };
-            if (digit(g, p)) |dp| {
-                if (dp > maxv) {
-                    maxp = p;
-                    maxv = dp;
-                }
-                // Nothing gonna be bigger than this.
-                if (maxv == 9) {
-                    break;
-                }
-            }
-        }
-        var max2p = maxp;
-        var max2v: u32 = 0;
-        for (@intCast(maxp[0] + 1)..1 + @as(usize, @intCast(g.bounds.maxs[0]))) |col| {
-            const p = .{ @as(i32, @intCast(col)), rowi };
-            if (digit(g, p)) |dp| {
-                if (dp > max2v) {
-                    max2p = p;
-                    max2v = dp;
-                }
-                // Nothing gonna be bigger than this.
-                if (max2v == 9) {
-                    break;
-                }
-            }
-        }
-        totes += (maxv * 10) + max2v;
+        totes += @intCast(select(g, .{ 0, @as(i32, @intCast(row)) }, 2, 0));
     }
-
     return totes;
 }
 
@@ -59,4 +52,24 @@ test "part1" {
     var g = try aoc.grid.openFileGrid(std.testing.allocator, "input/2025/day3");
     defer g.deinit();
     try std.testing.expectEqual(17100, part1(g.grid));
+}
+
+fn part2(g: aoc.grid.Grid) u64 {
+    var totes: u64 = 0;
+    for (0..1 + @as(usize, @intCast(g.bounds.maxs[1]))) |row| {
+        totes += select(g, .{ 0, @as(i32, @intCast(row)) }, 12, 0);
+    }
+    return totes;
+}
+
+test "part2ex" {
+    var g = try aoc.grid.openFileGrid(std.testing.allocator, "input/2025/day3.ex");
+    defer g.deinit();
+    try std.testing.expectEqual(3121910778619, part2(g.grid));
+}
+
+test "part2" {
+    var g = try aoc.grid.openFileGrid(std.testing.allocator, "input/2025/day3");
+    defer g.deinit();
+    try std.testing.expectEqual(170418192256861, part2(g.grid));
 }
